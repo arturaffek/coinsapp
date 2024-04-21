8adea8ba9f64504852e1931c7053fee3de80bd68
arturaffek <31987267+arturaffek@users.noreply.github.com>
2024-04-21 19:30:07 +0200

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-   port: process.env.PORT,
-   database: process.env.DATABASE
+   port: process.env.PORT||3001,
+   database: process.env.DATABASE||mongodb+srv://hello:kjhTScaTvQtFIP9k@cluster0.dsz30a5.mongodb.net/?retryWrites=true&w=majority
 }
